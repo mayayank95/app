2fa04b5844dab7a7fbacfe2bdcba1380adc847e6
mayayank95 <100045332+mayayank95@users.noreply.github.com>
2022-07-06 00:10:37 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -55,23 +55,5 @@ def main():
             st.pyplot(plt)
             st.write(end_time_2)
 
-
-@st.cache
-def gpu_memory_init():
-    # tf.debugging.set_log_device_placement(True)
-    # Below code is for "failed to create cublas handle: CUBLAS_STATUS_ALLOC_FAILED"
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
-
-
 if __name__ == "__main__":
     main()
