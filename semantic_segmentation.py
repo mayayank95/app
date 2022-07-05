7e4519f8e3bc7468d42a15eb80654d252f423001
mayayank95 <100045332+mayayank95@users.noreply.github.com>
2022-07-06 00:16:36 +0300

    Update semantic_segmentation.py

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -10,15 +10,15 @@ from PIL import Image as im
 def do_semantic(img_path):
     # absolute_path = os.path.abspath("model/deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
     absolute_path = os.path.abspath("cce_dice_loss_pretrained_model.h5")
-    # img = cv2.imread(img_path)
-    img = mpimg.imread(img_path)
-    img = resize(img, (256, 256)) 
-#   img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_NEAREST)  # [:,:,0]
+    img = cv2.imread(img_path)
+    #img = mpimg.imread(img_path)
+    #img = resize(img, (256, 256)) 
+   img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_NEAREST)  # [:,:,0]
 
     # segment_image = semantic_segmentation()
-#     model = tf.keras.models.load_model(absolute_path)
-#     x = img.reshape(1, 256, 256, 1)
-#     y = model.predict(x)
+    model = tf.keras.models.load_model(absolute_path)
+    x = img.reshape(1, 256, 256, 1)
+    y = model.predict(x)
     # Load the xception model trained on pascal voc for segmenting objects.
     # segment_image.load_pascalvoc_model(absolute_path)
     # segment_image.load_pascalvoc_model("pascal.h5")
@@ -28,7 +28,7 @@ def do_semantic(img_path):
     #                                                       overlay=True)
 
     # return segoverlay
-    return img,img#y[0, ...].argmax(axis=2), img
+    return y[0, ...].argmax(axis=2), img
 
 # output, segmap = segment_image.segmentAsPascalvoc("sample1.jpg")
 # cv2.imwrite("img.jpg", segoverlay)
